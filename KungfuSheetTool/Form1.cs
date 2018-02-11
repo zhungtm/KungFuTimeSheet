6b8bbf368642e09d636fe46c7c1704eb9e496a0b
Daniel Zhuang <danielzhuang11@hotmail.com>
2018-02-11 16:00:27 -0600

    find student method

--- KungfuSheetTool/Form1.cs
+++ KungfuSheetTool/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
 
 namespace KungfuSheetTool
 {
@@ -23,7 +24,7 @@ namespace KungfuSheetTool
             exlApp = new Microsoft.Office.Interop.Excel.Application();
             exlApp.Visible = false;
            
-            string sDataFile = "D:\\tomwork\\KungfuTimeSheet\\timesheet.xlsx";
+            string sDataFile = "D:\\DanielCode\\KungFuTool\\KungFuTimeSheet\\KungfuSheetTool\\bin\\Debug\\timesheet.xlsx";
 
             exlWB = exlApp.Workbooks.Open(sDataFile, Type.Missing, Type.Missing, Type.Missing,
                     Type.Missing, Type.Missing, Type.Missing, Type.Missing,
@@ -46,9 +47,25 @@ namespace KungfuSheetTool
             }          
         }
 
+        private int findStudent(string name)
+        {
+            for (int i = 2; i < studentCnt + 2; i++)
+            {
+                Microsoft.Office.Interop.Excel.Range rng = (Microsoft.Office.Interop.Excel.Range)exlWS.Cells[1, i];
+                string tmpName = (string)rng.Value2;
+                if (tmpName.Contains(name))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+
+        }
+
         private void btnSummary_Click(object sender, EventArgs e)
         {
-            string studentName = cbName.SelectedText;
+            string studentName = cbName.SelectedItem.ToString();
             DateTime currentDate = DateTime.Now;
             string currentDateStr = currentDate.ToString("MMMM dd, yyyy");
 
@@ -60,6 +77,11 @@ namespace KungfuSheetTool
             string dateStr;
             int cnt=0;
             string foundDateStr = "";
+
+            int column = 0;
+            // find this student's correspond column in timesheet
+            column = findStudent(studentName);
+          
             while (true)
             {
                 loctionStr = "A"+(cnt+2).ToString();
@@ -67,8 +89,10 @@ namespace KungfuSheetTool
                 double d = double.Parse(dateStr);
                 DateTime conv = DateTime.FromOADate(d);
                 dateStr = conv.ToString("MMMM dd, yyyy");
-                loctionStr = "B"+(cnt+2).ToString();
-                if(exlWS.Range[loctionStr].Formula=="x")
+                //loctionStr = "C"+(cnt+2).ToString();
+                Microsoft.Office.Interop.Excel.Range rng = (Microsoft.Office.Interop.Excel.Range)exlWS.Cells[cnt+2, column];
+                //if(exlWS.Range[loctionStr].Formula=="x")
+                if(rng.Formula=="x")
                 {
                     foundDateStr = dateStr;
                 }
